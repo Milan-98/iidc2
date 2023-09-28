d1c4e70ab00c4d8160d7854ed44f2b83a767c2bf
Milan-98 <milanparmar.me@gmail.com>
2023-09-28 16:42:09 +0530

    Align line with text-center

--- components/HeroOfAllPages.tsx
+++ components/HeroOfAllPages.tsx
@@ -1,5 +1,5 @@
 import { Poppins } from "next/font/google";
-const poppinsBold = Poppins({ weight: "700", subsets:["latin"] });
+const poppinsBold = Poppins({ weight: "700", subsets: ["latin"] });
 interface HeroOfAllPagesProps {
   heading: string;
   content: string;
@@ -9,11 +9,20 @@ const HeroOfAllPages: React.FC<HeroOfAllPagesProps> = ({
   content,
 }) => {
   return (
-    <section className="bg-[#00000048] bg-fixed bg-repeat-y bg-HeroOfItinerary bg-cover bg-left  px-6 pb-14 pt-28 text-white bg-blend-multiply md:bg-left md:px-16 md:pb-16 lg:bg-left-top lg:px-24 lg:py-40">
-      <h3 className={` text-center text-4xl font-black -tracking-tighter ${poppinsBold.className}`}>
+    <section className="bg-[#00000048] bg-HeroOfItinerary bg-cover bg-fixed bg-left bg-repeat-y  px-6 pb-14 pt-28 text-white bg-blend-multiply md:bg-left md:px-16 md:pb-16 lg:bg-left-top lg:px-24 lg:py-40">
+      <h3
+        className={` text-center text-4xl font-black -tracking-tighter ${poppinsBold.className}`}
+      >
         {heading}
       </h3>
-      <p className="mt-2 text-center md:mt-3 lg:text-lg ">{content}</p>
+      <p className="mt-2 text-center md:mt-3 lg:text-lg ">
+        {content.split("\n").map((line, index) => (
+          <span key={index}>
+            {line.trim()} {/* Add .trim() to remove leading/trailing spaces */}
+            {index < content.split("\n").length - 1 && <br />}
+          </span>
+        ))}
+      </p>
     </section>
   );
 };
